268d3f08d593cbcded50d65b48c1c0613058a81c
Kevin Ang <51168758+kvnang@users.noreply.github.com>
2023-04-21 05:54:28 -0600

    fix: metadatabase

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,6 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
-  ? new URL(`https://${process.env.NEXT_PUBLIC_BASE_URL}`)
+const ENV_URL =
+  process.env.NEXT_PUBLIC_BASE_URL || process.env.NEXT_PUBLIC_VERCEL_URL;
+
+export const BASE_URL = ENV_URL
+  ? new URL(`https://${ENV_URL}`)
   : new URL("http://localhost:3000");
